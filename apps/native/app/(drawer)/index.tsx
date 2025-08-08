d156fc0343f4034347cd3f4cf6ea785d3be7e8d3
Draco <thatdracoyt@gmail.com>
2025-08-09 01:11:07 +0500

    Initial commit

---
+++ apps/native/app/(drawer)/index.tsx
@@ -0,0 +1,15 @@
+import { ScrollView, Text, View } from "react-native";
+import { Container } from "@/components/container";
+
+export default function Home() {
+  return (
+    <Container>
+      <ScrollView showsVerticalScrollIndicator={false} className="flex-1">
+        <Text className="mb-4 font-bold font-mono text-3xl text-foreground">
+          BETTER T STACK
+        </Text>
+        <View className="mb-6 rounded-xl border border-border bg-card p-6 shadow-sm" />
+      </ScrollView>
+    </Container>
+  );
+}
